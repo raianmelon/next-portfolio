db4acddb9f6b2dca7e046670749c3f59d72aa30b
Raian Melon <rainamelon11@gmail.com>
2024-04-27 15:09:10 +0200

    SEO hidden

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,13 @@ import "./globals.css";
 import NavBar from "@/components/NavBar";
 import TitleHandler from "@/components/TitleHandler";
 import Footer from "@/components/Footer";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Raian Melon | Izrada web stranica",
-  description: "Transformiranje ideja u web stranice.",
+  // description: "Transformiranje ideja u web stranice.",
 };
 
 export default function RootLayout({
@@ -19,14 +20,17 @@ export default function RootLayout({
 }>) {
     return (
     <html lang="en">
-      <body className={inter.className + ' bg-background text-white'}>
-      <TitleHandler DefaultTitle={metadata.title}>
-          <NavBar/>
-          <main className={'min-h-[calc(100vh-90px)] overflow-x-hidden'}>
-              {children}
-          </main>
-          <Footer/>
-      </TitleHandler>
+    <Head>
+        <meta name="robots" content="noindex, nofollow"/>
+    </Head>
+    <body className={inter.className + ' bg-background text-white'}>
+        {/*<TitleHandler DefaultTitle={metadata.title}>*/}
+              <NavBar/>
+              <main className={'min-h-[calc(100vh-90px)] overflow-x-hidden'}>
+                  {children}
+              </main>
+              <Footer/>
+        {/*</TitleHandler>*/}
       </body>
     </html>
   );
