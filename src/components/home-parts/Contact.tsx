f7b946cbc07d3447dc12891ced392db95d09c450
Raian Melon <rainamelon11@gmail.com>
2024-04-29 20:30:12 +0200

    Iphone border fix 2nd try

--- src/components/home-parts/Contact.tsx
+++ src/components/home-parts/Contact.tsx
@@ -88,28 +88,28 @@ export default function Contact() {
                         <input value={name} onChange={(e) => {
                             setName(e.target.value)
                         }}
-                               className={`w-1/2 text-white border-r-0 bg-background border-b-2 ${errors.find((error) => error.for === "name") ? 'border-b-red-500' : 'border-b-white'} px-2 py-2 outline-0 focus:border-yellow transition-all`}
+                               className={`w-1/2 text-white border-r-0 bg-background border-b-2 ${errors.find((error) => error.for === "name") ? 'border-b-red-500' : 'border-b-white'} border-r-0 px-2 py-2 outline-0 focus:border-yellow transition-all`}
                                placeholder={'Ime'} type="text"/>
                         <input value={surname} onChange={(e) => {
                             setSurname(e.target.value)
                         }}
-                               className={`w-1/2 text-white border-r-0 bg-background border-b-2 ${errors.find((error) => error.for === "surname") ? 'border-b-red-500' : 'border-b-white'} px-2 py-2 outline-0 focus:border-yellow transition-all`}
+                               className={`w-1/2 text-white border-r-0 bg-background border-b-2 ${errors.find((error) => error.for === "surname") ? 'border-b-red-500' : 'border-b-white'} border-r-0 px-2 py-2 outline-0 focus:border-yellow transition-all`}
                                placeholder={'Prezime'} type="text"/>
                     </div>
                     <input value={email} onChange={(e) => {
                         setEmail(e.target.value)
                     }}
-                           className={`text-white border-r-0 bg-background border-b-2 ${errors.find((error) => error.for === "email") ? 'border-b-red-500' : 'border-b-white'} px-2 py-2 outline-0 focus:border-yellow transition-all w-full`}
+                           className={`text-white border-r-0 bg-background border-b-2 ${errors.find((error) => error.for === "email") ? 'border-b-red-500' : 'border-b-white'} border-r-0 px-2 py-2 outline-0 focus:border-yellow transition-all w-full`}
                            placeholder={'E-mail'} type="text"/>
                     <input value={contactReason} onChange={(e) => {
                         setContactReason(e.target.value)
                     }}
-                           className={`text-white border-r-0 bg-background border-b-2 ${errors.find((error) => error.for === "contactReason") ? 'border-b-red-500' : 'border-b-white'} px-2 py-2 outline-0 focus:border-yellow transition-all w-full`}
+                           className={`text-white border-r-0 bg-background border-b-2 ${errors.find((error) => error.for === "contactReason") ? 'border-b-red-500' : 'border-b-white'} border-r-0 px-2 py-2 outline-0 focus:border-yellow transition-all w-full`}
                            placeholder={'Razlog kontaktiranja'} type="text"/>
                     <textarea value={message} onChange={(e) => {
                         setMessage(e.target.value)
                     }}
-                              className={`h-32 text-white bg-background border-2 ${errors.find((error) => error.for === "message") ? 'border-b-red-500' : 'border-b-white'} px-4 py-2 outline-0 focus:border-yellow transition-all w-full rounded-md`}
+                              className={`h-32 text-white bg-background border-2 ${errors.find((error) => error.for === "message") ? 'border-b-red-500' : 'border-b-white'} border-r-0 px-4 py-2 outline-0 focus:border-yellow transition-all w-full rounded-md`}
                               placeholder={'Poruka'}/>
                     {responseMessage &&
                         <p className={`text-white rounded-md ${responseMessageStatus == 200 ? 'bg-[#298c43] border-[#298c43]' : 'border-[#8c2929] bg-[#8c2929]'} bg-opacity-30 border-2 px-4 py-3 w-full`}>
