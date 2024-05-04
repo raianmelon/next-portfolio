2d40e7d39d563aa1e9cafc0cfef5be79da964599
Raian Melon <rainamelon11@gmail.com>
2024-05-04 18:18:53 +0200

    Services, new text

--- src/components/home-parts/Services.tsx
+++ src/components/home-parts/Services.tsx
@@ -1,4 +1,4 @@
-import {PencilLine, ShoppingBasket, Wrench} from "lucide-react";
+import {AppWindow, MonitorSmartphone, Paintbrush, ShoppingBasket, Wrench} from "lucide-react";
 import {AnimatedHeading} from "@/components/AnimatedElement";
 
 export default function Services() {
@@ -10,33 +10,33 @@ export default function Services() {
                 <div
                     className={'w-full lg:max-w-[25%] h-fit rounded-xl bg-yellow bg-opacity-80 hover:bg-opacity-100 transition-all text-black px-6 py-8 flex flex-col gap-10 justify-center items-center'}>
                     <div className={'flex flex-col gap-5 justify-center items-center'}>
-                        <PencilLine size={40}/>
-                        <h1 className={'text-2xl font-bold'}>Blog</h1>
+                        <Paintbrush size={40}/>
+                        <h1 className={'text-2xl font-bold text-center'}>Web dizajn</h1>
                     </div>
-                    <p className={'text-md leading-8'}>Nudim kvalitetnu izradu blog web stranica prilagođenih vašem
-                        sadržaju, s elegantnim dizajnom i optimizacijom za pretraživače, pomažući vam u izgradnji
-                        vjerne publike i autoriteta u vašoj mjeri.</p>
+                    <p className={'text-md leading-8 text-'}>
+                        Nudim vrhunsko korisničko iskustvo za vašu web stranicu, bez obzira na to je li riječ o personaliziranoj web lokaciji ili e-trgovini. Moja stručnost u dizajnu korisničkog sučelja i iskustva (UI/UX) osigurava intuitivnost, privlačnost i prilagođenost vašim ciljevima.</p>
                 </div>
                 <div
                     className={'w-full lg:max-w-[30%] rounded-xl bg-yellow hover:bg-opacity-80 transition-all text-black px-6 py-8 flex flex-col gap-10 justify-center items-center'}>
                     <div className={'flex flex-col gap-5 justify-center items-center'}>
-                        <ShoppingBasket size={50}/>
-                        <h1 className={'text-2xl font-bold'}>Web Shop</h1>
+                        <MonitorSmartphone size={50}/>
+                        <h1 className={'text-2xl font-bold text-center'}>Web stranica + Web Dizajn</h1>
                     </div>
-                    <p className={'text-md leading-8'}>Nudim potpunu uslugu izrade webshopova prilagođenih vašem
-                        brendu, s naglaskom na personaliziranom dizajnu, intuitivnoj funkcionalnosti, optimizaciji za
-                        pretraživače te kontinuiranoj podršci i održavanju, osiguravajući vam konkurentsku prednost i
-                        uspjeh na digitalnom tržištu.</p>
+                    <p className={'text-md leading-8'}>Moja usluga izrade web stranica i dizajna kombinira stručnost u
+                        dizajnu i tehnologiji kako bi stvorila online prisutnost koja vas ističe u digitalnom svijetu.
+                        Svaka stranica koju kreiram je prilagođena vašim potrebama i brendu, s fokusom na responsivni
+                        dizajn, SEO optimizaciji i korisničkom iskustvu. Kontaktirajte me i dopustite mi da vašu viziju
+                        pretvorimo u stvarnost na webu.</p>
                 </div>
                 <div
                     className={'w-full lg:max-w-[25%] h-fit rounded-xl bg-opacity-80 hover:bg-opacity-100 transition-all shadow-gray bg-yellow text-black px-6 py-8 flex flex-col gap-10 justify-center items-center'}>
                     <div className={'flex flex-col gap-5 justify-center items-center'}>
-                        <Wrench size={40}/>
-                        <h1 className={'text-2xl font-bold'}>Prilagođena Web Stranica</h1>
+                        <AppWindow size={40}/>
+                        <h1 className={'text-2xl font-bold text-center'}>Web stranica</h1>
                     </div>
-                    <p className={'text-md leading-8'}>Nudim izradu prilagođenih web stranica, odražavajući vaše
-                        potrebe i estetiku, uz naglasak na funkcionalnosti i korisničkom iskustvu, kako biste
-                        ostvarili svoje poslovne ciljeve na webu.</p>
+                    <p className={'text-md leading-8'}>Moj usluga izrade web stranica s pripremljenim dizajnom korisnika
+                        omogućuje vam brzo pokretanje online prisutnosti s minimalnim naporom. Koristeći vaš
+                        pripremljeni dizajn, stvaram web stranicu koja odražava vaš brend i identitet.</p>
                 </div>
             </div>
         </section>
