c131db71ef721855f2746b4991b7b8291b9d68fa
Raian Melon <rainamelon11@gmail.com>
2024-05-08 19:50:40 +0200

    not found style fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,13 +3,13 @@ import {FilledButtonWithOutline} from "@/components/Buttons";
 export default function NotFound() {
     return (
         <section className={'px-4 md:px-16 xl:px-32 2xl:px-52 pb-10 lg:py-28 flex justify-center items-center flex-col h-screen'}>
-            <div className={'text-[12rem] flex justify-center items-center text-yellow'}>
-                <h1 className={'pt-20'}>4</h1>
+            <div className={'text-[8rem] md:text-[12rem] flex justify-center items-center text-yellow'}>
+                <h1 className={'pt-10 md:pt-20'}>4</h1>
                 <span className={'text-white'}>0</span>
-                <h1 className={'pb-20'}>4</h1>
+                <h1 className={'pb-10 md:pb-20'}>4</h1>
             </div>
-            <p className={'text-2xl'}>Nažalost, ta stranica ne postoji.</p>
-            <FilledButtonWithOutline className={'mt-10 text-md'} href="/">Vratite se na početnu</FilledButtonWithOutline>
+            <p className={'text-xl mdtext-2xl text-center'}>Nažalost, ta stranica ne postoji.</p>
+            <FilledButtonWithOutline className={'mt-10'} href="/">Vratite se na početnu</FilledButtonWithOutline>
         </section>
     )
 }
