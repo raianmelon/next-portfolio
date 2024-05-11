3402c0e3c26b60c296c49ad6e5ef2a5ae3d8a62a
Raian Melon <rainamelon11@gmail.com>
2024-05-11 18:57:33 +0200

    set logo to not lazy loading

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -116,7 +116,7 @@ export default function NavBar() {
                 setIsMenuOpen(false)
                 setIsMenuOpenWithNoDelay(false)
             }} className={'flex justify-center items-center gap-5'}>
-                <Image className={'rounded-full hover:ring-[2.5px] ring-yellow transition-all'} src={'logo.svg'} alt={'Logo Raian Melon'} width={50} height={50} />
+                <Image loading={'eager'} className={'rounded-full hover:ring-[2.5px] ring-yellow transition-all'} src={'logo.svg'} alt={'Logo Raian Melon'} width={50} height={50} />
                 <span className={'flex text-lg font-bold gap-0.5 group transition-all py-2.5'}><h1 className={'text-yellow inline-block align-middle group-hover:text-white transition-all'}>Raian</h1><h1 className={'text-white inline-block align-middle group-hover:text-yellow transition-all'}>Melon</h1></span>
             </Link>
 
