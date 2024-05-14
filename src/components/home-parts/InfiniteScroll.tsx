9183fdc0de20889cfb5d025d064cb3f853892f55
Raian Melon <rainamelon11@gmail.com>
2024-05-14 20:32:10 +0200

    infinit slider change

--- src/components/home-parts/InfiniteScroll.tsx
+++ src/components/home-parts/InfiniteScroll.tsx
@@ -2,7 +2,7 @@
 
 export default function InfiniteScroll() {
     return (
-        <section className={'px-4 md:px-10 mt-20 lg:px-20 text-lg lg:text-xl h-20 w-full text-white shadow-[0px_0px_10px_2px_#FED700] opacity-50'}>
+        <section className={'px-4 md:px-10 mt-0 sm:mt-20 lg:px-20 text-lg lg:text-xl h-20 w-full text-white shadow-[0px_0px_10px_2px_#FED700] opacity-50'}>
             <div className={'w-full h-full overflow-hidden'}>
                 <div className={'flex gap-5 items-center justify-center h-full w-max'} style={{animation: 'infinite-scroll 40s linear forwards', animationTimeline: 'scroll(root)'}}>
                     <div className={'flex gap-5 items-center justify-center w-max -z-10'}>
--- src/components/home-parts/Skills.tsx
+++ src/components/home-parts/Skills.tsx
@@ -2,33 +2,33 @@ import Image from "next/image";
 
 export default function Skills() {
     return (
-        <section id={'skills'} className={'px-4 md:px-16 xl:px-32 2xl:px-52 py-10 lg:py-28'}>
+        <section id={'skills'} className={'px-4 md:px-16 xl:px-32 2xl:px-52 py-10'}>
             <div className={'w-full mt-10 lg:mt-20 h-full overflow-hidden'}>
                 <div className={'flex gap-10 lg:gap-24 items-center justify-center h-full w-max'}
                      style={{animation: 'infinite-scroll2 30s linear infinite'}}>
-                    <div className={'flex gap-10 lg:gap-24 items-center justify-center w-max -z-10'}>
-                        <Image src={'/html.png'} alt={'html'} width={70} height={70}/>
-                        <Image src={'/css.png'} alt={'css'} width={70} height={79}/>
-                        <Image src={'/tailwind.png'} alt={'tailwind'} width={70} height={42}/>
-                        <Image src={'/javascript.png'} alt={'javascript'} width={70} height={70}/>
-                        <Image src={'/typescript.png'} alt={'typescript'} width={70} height={70}/>
-                        <Image src={'/node.png'} alt={'node'} width={70} height={32}/>
-                        <Image src={'/svelte.png'} alt={'svelte'} width={70} height={84}/>
-                        <Image src={'/react.png'} alt={'react'} width={70} height={61}/>
-                        <Image src={'/nextjs.svg'} alt={'nextjs'} width={70} height={70}/>
-                        <Image src={'/prisma.png'} alt={'prisma'} width={100} height={41}/>
+                    <div className={'flex gap-10 sm:gap-24 items-center justify-center w-max -z-10'}>
+                        <Image src={'/html.png'} alt={'html'} className={'h-[50px] w-auto sm:h-auto sm:w-[70px]'} width={70} height={70}/>
+                        <Image src={'/css.png'} alt={'css'} className={'h-[50px] w-auto sm:h-auto sm:w-[70px]'} width={70} height={79}/>
+                        <Image src={'/tailwind.png'} alt={'tailwind'} className={'w-[50px] h-auto sm:h-auto sm:w-[70px]'} width={70} height={42}/>
+                        <Image src={'/javascript.png'} alt={'javascript'} className={'h-[50px] w-auto sm:h-auto sm:w-[70px]'} width={70} height={70}/>
+                        <Image src={'/typescript.png'} alt={'typescript'} className={'h-[50px] w-auto sm:h-auto sm:w-[70px]'} width={70} height={70}/>
+                        <Image src={'/node.png'} alt={'node'} className={'h-[50px] w-auto sm:h-auto sm:w-[70px]'} width={70} height={32}/>
+                        <Image src={'/svelte.png'} alt={'svelte'} className={'h-[50px] w-auto sm:h-auto sm:w-[70px]'} width={70} height={84}/>
+                        <Image src={'/react.png'} alt={'react'} className={'h-[50px] w-auto sm:h-auto sm:w-[70px]'} width={70} height={61}/>
+                        <Image src={'/nextjs.svg'} alt={'nextjs'} className={'h-[50px] w-auto sm:h-auto sm:w-[70px]'} width={70} height={70}/>
+                        <Image src={'/prisma.png'} alt={'prisma'} className={'h-[50px] w-auto sm:h-[70px] sm:w-auto'} width={100} height={41}/>
                     </div>
                     <div className={'w-max flex gap-10 lg:gap-24 items-center justify-center'}>
-                        <Image src={'/html.png'} alt={'html'} width={70} height={70}/>
-                        <Image src={'/css.png'} alt={'css'} width={70} height={79}/>
-                        <Image src={'/tailwind.png'} alt={'tailwind'} width={70} height={42}/>
-                        <Image src={'/javascript.png'} alt={'javascript'} width={70} height={70}/>
-                        <Image src={'/typescript.png'} alt={'typescript'} width={70} height={70}/>
-                        <Image src={'/node.png'} alt={'node'} width={70} height={32}/>
-                        <Image src={'/svelte.png'} alt={'svelte'} width={70} height={84}/>
-                        <Image src={'/react.png'} alt={'react'} width={70} height={61}/>
-                        <Image src={'/nextjs.svg'} alt={'nextjs'} width={70} height={70}/>
-                        <Image src={'/prisma.png'} alt={'prisma'} width={100} height={41}/>
+                        <Image src={'/html.png'} alt={'html'} className={'h-[50px] w-auto sm:h-auto sm:w-[70px]'} width={70} height={70}/>
+                        <Image src={'/css.png'} alt={'css'} className={'h-[50px] w-auto sm:h-auto sm:w-[70px]'} width={70} height={79}/>
+                        <Image src={'/tailwind.png'} alt={'tailwind'} className={'w-[50px] h-auto sm:h-auto sm:w-[70px]'} width={70} height={42}/>
+                        <Image src={'/javascript.png'} alt={'javascript'} className={'h-[50px] w-auto sm:h-auto sm:w-[70px]'} width={70} height={70}/>
+                        <Image src={'/typescript.png'} alt={'typescript'} className={'h-[50px] w-auto sm:h-auto sm:w-[70px]'} width={70} height={70}/>
+                        <Image src={'/node.png'} alt={'node'} className={'h-[50px] w-auto sm:h-auto sm:w-[70px]'} width={70} height={32}/>
+                        <Image src={'/svelte.png'} alt={'svelte'} className={'h-[50px] w-auto sm:h-auto sm:w-[70px]'} width={70} height={84}/>
+                        <Image src={'/react.png'} alt={'react'} className={'h-[50px] w-auto sm:h-auto sm:w-[70px]'} width={70} height={61}/>
+                        <Image src={'/nextjs.svg'} alt={'nextjs'} className={'h-[50px] w-auto sm:h-auto sm:w-[70px]'} width={70} height={70}/>
+                        <Image src={'/prisma.png'} alt={'prisma'} className={'h-[50px] w-auto sm:h-[70px] sm:w-auto'} width={100} height={41}/>
                     </div>
                 </div>
             </div>
